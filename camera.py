cb378313ad20921671702a571ec26456183c6646
Noob-Coder-CS <chunpotter@gmail.com>
2017-05-14 13:35:57 +0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,11 +3,11 @@ from picamera import PiCamera
 camera = PiCamera()
 camera.capture('/home/pi/Desktop/window.jpg')
 
-from google.cloud import storage
+from google.cloud import storage #is this pre-build in the pi already? after you get-apt and configure the pi, CS
 
-client = storage.Client()
+client = storage.Client() #where does Client come from?, CS
 
-bucket = client.get_bucket('smartwindow-91b1c.appspot.com')
+bucket = client.get_bucket('smartwindow-91b1c.appspot.com'), 
 
-imageBlob = bucket.get_blob('window.jpg')
+imageBlob = bucket.get_blob('window.jpg') 
 imageBlob.upload_from_filename(filename='/home/pi/Desktop/window.jpg')
